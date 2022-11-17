f982cf2eccf05783c55bd8aa8293534c4fa44042
Amir Fazliev <fazliev.amir@yandex.ru>
2022-11-17 18:37:20 +0300

    change java file

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,58 @@
+package org.example;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.opencsv.CSVReader;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
+        String fileName = "data.csv";
+
+        List<Employee> list = parseCSV(columnMapping, fileName);
+        String json = listToJson(list);
+
+        writeString(json);
+    }
+
+    public static List<Employee> parseCSV(String[] columnMap, String file) {
+        try (CSVReader csvReader = new CSVReader(new FileReader(file))) {
+            ColumnPositionMappingStrategy<Employee> strategy = new ColumnPositionMappingStrategy<>();
+            strategy.setType(Employee. class);
+            strategy.setColumnMapping(columnMap);
+            CsvToBean<Employee> csb = new CsvToBeanBuilder<Employee>(csvReader)
+                    .withMappingStrategy(strategy)
+                    .build();
+            return csb.parse();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String listToJson (List<Employee> list) {
+        GsonBuilder gb = new GsonBuilder();
+        Gson gson = gb.create();
+        Type listType = new TypeToken<List<Employee>>(){}.getType();
+        return gson.toJson(list, listType);
+    }
+
+    public static void writeString (String json) {
+        try (FileWriter file = new
+                FileWriter("data.json")) {
+            file.write(json);
+            file.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
